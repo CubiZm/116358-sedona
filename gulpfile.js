31ee3a000251b81468e3eb986d8fca9a668cbfac
Anna Karpina <ani-mullen@yandex.ru>
2016-06-25 10:48:34 +0300

    module6

--- gulpfile.js
+++ gulpfile.js
@@ -31,3 +31,6 @@ gulp.task("serve", ["style"], function() {
     open: true,
     ui: false
   });
+  gulp.watch("postcss/**/*.css", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
